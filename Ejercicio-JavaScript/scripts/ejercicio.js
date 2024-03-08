2ba807dbc6ff0876762414aad3da63f01f63f8ad
MisssChipi <162648763+MisssChipi@users.noreply.github.com>
2024-03-07 19:55:37 -0700

    Add files via upload

---
+++ Ejercicio-JavaScript/scripts/ejercicio.js
@@ -0,0 +1,10 @@
+variable_album=document.getElementById('album');
+function CambiarColor(){
+    console.log(document.getElementById('album'));
+    variable_album.style.color="white";
+    document.body.style.backgroundColor="black";
+}
+function cambiarTamano() {
+    var imagen = document.getElementsByClassName("imagen");
+    imagen[0].style.height="500px";
+}
